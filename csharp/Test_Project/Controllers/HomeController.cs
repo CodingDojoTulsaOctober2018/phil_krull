bef183289bc81d3ca6df4ef186a3729a2cc3d34b
Phil Krull <pkrull@codingdojo.com>
2019-01-14 13:57:55 -0600

    adding session to books and authors

--- csharp/Test_Project/Controllers/HomeController.cs
+++ csharp/Test_Project/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Test_Project.Models;
 
+
 namespace Test_Project.Controllers
 {
     public class HomeController : Controller
@@ -13,7 +14,32 @@ namespace Test_Project.Controllers
         [Route("")]
         public IActionResult Index()
         {
+            // add in authors to session
+
+            Console.WriteLine($"++++++++++++++++++++ Author from Index Method {TempData["author"]}++++++++++++++++++++");
             ViewModel AuthorView = new ViewModel();
+
+            string AuthorChecker = HttpContext.Session.GetString("AllAuthors");
+
+            if(AuthorChecker == null) {
+                // Save the allAuthors in session
+                HttpContext.Session.SetObjectAsJson("AllAuthors", AuthorView.Allauthors);
+            } else {
+                // all authors is set already, add the added author to session
+                // get current list of authrs from session
+                List<Author> AllAuthors = HttpContext.Session.GetObjectFromJson<List<Author>>("AllAuthors");
+                // add the new author to the list
+                Author NewAuthor = new Author((string)TempData["author"]);
+                AllAuthors.Add(NewAuthor);
+                // save the new list in the viewmodel
+                AuthorView.Allauthors = AllAuthors;
+                // save the the list in session
+                HttpContext.Session.SetObjectAsJson("AllAuthors", AllAuthors);
+            }
+
+
+
+
             return View(AuthorView);
         }
 
@@ -24,6 +50,7 @@ namespace Test_Project.Controllers
             if(ModelState.IsValid) {
                 Console.WriteLine($"------------------------ Author name is: {author.Name} -------------------------");
                 Console.WriteLine(author.Name);
+                TempData["author"] = author.Name;
                 return RedirectToAction("Index");
             } else {
                 ViewModel AuthorView = new ViewModel();
@@ -31,5 +58,6 @@ namespace Test_Project.Controllers
                 return View("Index", AuthorView);
             }
         }
+
     }
 }
